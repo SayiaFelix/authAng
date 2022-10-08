79390fda553d6775613c13357c2e5cee8ed6f8a1
SayiaFelix <sayiafelix18@gmail.com>
2022-10-08 08:50:03 +0300

    coding login functions

--- AuthUI/src/app/login/login.component.ts
+++ AuthUI/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  form:FormGroup
 
-  constructor() { }
+  constructor(private fb:FormBuilder,private authService:AuthService,private router:Router) { 
+
+    this.form= this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+login(){
+  const val = this.form.value;
+
+  if (val.email && val.password){
+    this.authService.login(val.email, val.password)
+    .subscribe(
+
+      ()=>{
+        console.log('user is logged in successful');
+        this.router.navigateByUrl('/')
+      }
+      
+    )
+  }
+}
+
+
+
 
   ngOnInit(): void {
   }
