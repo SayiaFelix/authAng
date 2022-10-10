925be75829bb73bacbec7edfb239111db4b73df6
SayiaFelix <sayiafelix18@gmail.com>
2022-10-10 10:32:53 +0300

    fixing bug

--- AuthUI/src/app/signup/signup.component.ts
+++ AuthUI/src/app/signup/signup.component.ts
@@ -23,4 +23,27 @@ export class SignupComponent implements OnInit {
 
 }
 
+
+
+
+
+
+private validateAllFormFields(formGroup:FormGroup)
+{
+Object.keys(formGroup.controls).forEach(field=>{
+  const control = formGroup.get(field);
+
+  if( control instanceof FormControl){
+    control.markAsDirty( {onlySelf: true});
+  }
+
+  else if( control instanceof FormGroup){
+    this.validateAllFormFields(control);
+  }
+
+
+})
+  
+}
+
 }
