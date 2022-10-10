dd0b9fa71403a33a892093ff96d707a2911003e5
SayiaFelix <sayiafelix18@gmail.com>
2022-10-10 10:31:43 +0300

    fixing bug

--- AuthUI/src/app/signup/signup.component.ts
+++ AuthUI/src/app/signup/signup.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup ,FormControl,Validator, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
 
-  constructor() { }
+export class SignupComponent implements OnInit {
+  SignupForm!: FormGroup; 
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    this.SignupForm= this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+  })
+
+}
 
 }
