9b01d7186f3431142c4732abf3acf520b9c909f7
SayiaFelix <sayiafelix18@gmail.com>
2022-10-11 12:08:04 +0300

    adding radio button

--- AuthUI/src/app/app.module.ts
+++ AuthUI/src/app/app.module.ts
@@ -19,7 +19,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-
+import {MatRadioModule} from '@angular/material/radio';
 
 
 
@@ -47,7 +47,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatInputModule,
     MatSelectModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
