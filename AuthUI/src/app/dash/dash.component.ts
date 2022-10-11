9dd5f68dbd6159339d975d3d3f2918b04fbceb52
SayiaFelix <sayiafelix18@gmail.com>
2022-10-11 10:51:11 +0300

    adding dialog component

--- AuthUI/src/app/dash/dash.component.ts
+++ AuthUI/src/app/dash/dash.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-dash',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+   
+    });
+  }
 
   ngOnInit(): void {
   }
