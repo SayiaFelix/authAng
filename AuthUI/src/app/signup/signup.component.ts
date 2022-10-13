72d1336b3575d7ce47939dfad84ca6a7db22e1a8
SayiaFelix <sayiafelix18@gmail.com>
2022-10-13 09:55:26 +0300

    styling signup

--- AuthUI/src/app/signup/signup.component.ts
+++ AuthUI/src/app/signup/signup.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup ,FormControl,Validator, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -9,7 +11,7 @@ import { FormBuilder, FormGroup ,FormControl,Validator, Validators} from '@angul
 
 export class SignupComponent implements OnInit {
   public SignupForm!: FormGroup; 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder,private http:HttpClient,private router:Router) { }
 
   ngOnInit(): void {
     this.SignupForm= this.fb.group({
@@ -24,6 +26,15 @@ export class SignupComponent implements OnInit {
 Signup(){
   if(this.SignupForm.valid){
     console.log(this.SignupForm.value);
+    this.http.post<any>("http://localhost:3000/signupusers/",this.SignupForm.value)
+    .subscribe(res=>{
+      alert('Signup successfully');
+      this.SignupForm.reset();
+     this.router.navigate(['login']);
+    },err=>{
+      alert('something went wrong')
+
+    })
     // send obj to db
 
   }else{
