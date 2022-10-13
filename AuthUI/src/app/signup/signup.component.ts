a7c880801ba7ba2286e6f4a257cad914e30d0fe9
SayiaFelix <sayiafelix18@gmail.com>
2022-10-13 09:57:09 +0300

    editing signup page

--- AuthUI/src/app/signup/signup.component.ts
+++ AuthUI/src/app/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup ,FormControl,Validator, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup ,FormControl, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
