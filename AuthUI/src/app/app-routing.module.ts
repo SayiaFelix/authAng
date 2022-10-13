9b77f9c36b04e56cd2f034d9ebdc09304f014e4b
SayiaFelix <sayiafelix18@gmail.com>
2022-10-13 08:44:35 +0300

    styling login

--- AuthUI/src/app/app-routing.module.ts
+++ AuthUI/src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
+  { path: '', redirectTo:"/login", pathMatch:"full"},
   {path:'login',component:LoginComponent},
   {path:'signup',component:SignupComponent},
-  {path:'dash',component:DashComponent},
+  {path:'dashboard',component:DashComponent},
   
-  { path: '', redirectTo:"/login", pathMatch:"full"},
 ];
 
 @NgModule({
